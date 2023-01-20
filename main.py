d5f4764244d38c8b33757bdfb8e60700f6719255
rodrigo.sauda <rodrigo.sauda@hotmail.com>
2023-01-20 17:48:38 +0100

    simple project about list comprehension

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv("nato_phonetic_alphabet.csv")
+nato_phonetic = df.set_index('letter')['code'].to_dict()
+print(nato_phonetic)
+
+user_word = input("Enter a word: ").upper()
+user_word_list = [word for word in user_word]
+
+result = [nato_phonetic[letter] for letter in user_word_list]
+
+print(result)
+
+
