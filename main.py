a5dfdac532acbc1b6b8bd8d47c531dae9ffc58ce
Rodrigo Ulbrich Sauda <109063514+rosauda@users.noreply.github.com>
2023-08-21 16:43:55 +0200

    adding error handler

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ nato_phonetic = df.set_index('letter')['code'].to_dict()
 print(nato_phonetic)
 
 while True:
-
     user_word = input("Enter a word: ").upper()
     try:
         user_word_list = [word for word in user_word]
         result = [nato_phonetic[letter] for letter in user_word_list]
-
         print(result)
         break
-
     except KeyError:
         print("Sorry, only letters in the alphabet please.")
 
